0181b488f893f05efc76d3fb0115bc18806663ed
EmmetHughes <0257695@schoolsnet.act.edu.au>
2025-02-14 09:25:12 +1100

    added bullet code to enemy
    still not working. Will be hopefully fixed

---
+++ Scenes/bullet.gd
@@ -0,0 +1,11 @@
+extends RigidBody3D
+
+@export var speed: float = 20.0
+
+func _ready():
+	# Apply an initial velocity in the forward direction
+	linear_velocity = transform.basis.z * -speed
+
+func _on_collision():
+	# Handle what happens when the bullet collides (e.g., damage player, destroy bullet)
+	queue_free()  # Destroy the bullet after collision
