4f0dbf071fad31b6a7e876ff11d88cf301e7db5c
UmairIsMe <159209470+UmairIsMe@users.noreply.github.com>
2025-03-26 12:23:21 +1100

    volume

--- options.gd
+++ options.gd
@@ -13,3 +13,8 @@ func _process(delta: float) -> void:
 
 func _on_return_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+
+func _on_volume_value_changed(value: float) -> void:
+	pass # Replace with function body.
+	AudioServer.set_bus_volume_db(0,value)
