980e181e15fbdac4484e23f03eca0c41e3882a4b
UmairIsMe <159209470+UmairIsMe@users.noreply.github.com>
2025-03-26 12:44:06 +1100

    volume + resilution

--- options.gd
+++ options.gd
@@ -18,3 +18,18 @@ func _on_return_pressed() -> void:
 func _on_volume_value_changed(value: float) -> void:
 	pass # Replace with function body.
 	AudioServer.set_bus_volume_db(0,value)
+
+
+func _on_mute_toggled(toggled_on: bool) -> void:
+	pass # Replace with function body.
+
+
+func _on_resolutions_item_selected(index: int) -> void:
+	pass # Replace with function body.
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920,1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1600,900))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280,720))
