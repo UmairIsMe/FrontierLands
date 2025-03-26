e8f767b70f7c08ae5629caadc56acbc2b0edd61b
AtharvaG0311 <4200717@schoolsnet.act.edu.au>
2025-03-26 12:19:43 +1100

    Fixed player shooting (direction)

---
+++ health_bar.gd
@@ -0,0 +1,15 @@
+extends ProgressBar
+
+@export var player: CharacterBody3D
+@onready var health_bar: ProgressBar = $HealthBar
+@onready var max_health
+
+
+
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
